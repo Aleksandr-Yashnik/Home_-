06cb8d3898af92512ddabcda80d7ff8f9ea3de3b
jashnik.aleksandr <jashnik.aleksandr@gmail.com>
2024-05-05 21:46:03 +0300

    добавил коменты

--- Lesson_18/index.js
+++ Lesson_18/index.js
@@ -129,42 +129,55 @@ const products = [
   },
 ];
 
+// Получаем ссылку на контейнер для отображения товаров
 const productsContainer = document.getElementById("products");
+// Получаем ссылку на выпадающий список для выбора пола
 const genderSelect = document.getElementById("genderSelect");
+// Получаем ссылку на выпадающий список для выбора товара
 const productSelect = document.getElementById("productSelect");
+// Получаем ссылку на кнопку "Search"
 const searchBtn = document.getElementById("searchBtn");
 
+// Функция для отображения товаров в соответствии с выбранными фильтрамим
 function renderProducts() {
+  // Получаем выбранный пол из выпадающего списка
   const selectedGender = genderSelect.value;
+  // Получаем выбранный товар из выпадающего списка
   const selectedProduct = productSelect.value;
 
+   // Очищаем контейнер перед отображением новых товаров
   productsContainer.innerHTML = "";
-
+ // Проходим по каждому товару в массиве
   products.forEach((product) => {
     if (
       (selectedGender === "all" || product.gender === selectedGender) &&
       (selectedProduct === "all" ||
         product.name.toLowerCase().includes(selectedProduct))
     ) {
+      // Создаем элемент карточки товара
       const productCard = document.createElement("div");
       productCard.classList.add("product-card");
+      // Создаем изображение товара и устанавливаем его источник
       const image = document.createElement("img");
       image.src = product.image;
+      // Создаем элементы названия, цены и статуса товара и устанавливаем их содержимое
       const name = document.createElement("h3");
       name.textContent = product.name;
       const price = document.createElement("p");
       price.textContent = `Price: ${product.price}`;
       const status = document.createElement("p");
       status.textContent = `Status: ${product.status}`;
+      // Добавляем созданные элементы в карточку товара
       productCard.appendChild(image);
       productCard.appendChild(name);
       productCard.appendChild(price);
       productCard.appendChild(status);
+       // Добавляем карточку товара в контейнер для отображения товаров
       productsContainer.appendChild(productCard);
     }
   });
 }
-
+// Добавляем обработчик события для кнопки "Search", который вызывает функцию отображения товаров
 searchBtn.addEventListener("click", renderProducts);
-
+// Вызываем функцию отображения товаров при загрузке страницы
 renderProducts();
